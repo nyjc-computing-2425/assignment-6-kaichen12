564ee046d18523c3217845cdc35b94e0cdcbb537
kaichen12 <160604150+kaichen12@users.noreply.github.com>
2024-03-30 13:18:54 +0000

    final submission

--- main.py
+++ main.py
@@ -4,17 +4,68 @@ import time
 
 # Part 1
 def clock(n):
+    """
+    parameter
+    ---------
+    n : int
+        any integer
+     
+    returns
+    -------
+    time in the form of (HH:MM:SS) which run for n seconds
+    """
     # Your code here
-    pass
-
+    Time_ = n
+    while Time_ >= 0:
+        currenttime = datetime.now().strftime('%H:%M:%S')
+        time.sleep(1)
+        print(currenttime,end='\r')
+        Time_ = Time_ - 1
+    
 
 # Part 2
 def lcm(a, b):
-    # Your code here
-    pass
+    """
+    parameter
+    ---------
+    a : int
+        any integer
+    b : int
+        any integer
 
+    returns
+    -------
+    lcm of a and b
+    """
+    a_original = a
+    b_original = b
+    # Your code here
+    while a != b:
+        if a < b:
+            a += a_original
+        elif b < a:
+            b += b_original   
+    if a == b:
+        return a
 
 # Part 3
 def gcf(a, b):
     # Your code here
-    pass
+    """
+    Parameter
+    ---------
+    a : int
+        any integer
+    b : int
+        any integer
+
+    returns
+    -------
+    gcf of a and b
+    """
+    while b != 0:
+        r = a % b
+        a = b
+        b = r
+    return a
+
